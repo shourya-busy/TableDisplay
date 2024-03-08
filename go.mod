cd858fa4d7a2d34624b88cfcbf4c67b7a4e97410
shouryagautam <shourya@busy.mail.in>
2024-03-08 18:14:04 +0530

    Implemented generic query using goqu

--- go.mod
+++ go.mod
@@ -5,9 +5,12 @@ go 1.21.6
 require github.com/go-pg/pg/v10 v10.12.0
 
 require (
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/bytedance/sonic v1.10.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.0 // indirect
+	github.com/doug-martin/goqu/v9 v9.19.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -17,6 +20,8 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
@@ -40,6 +45,7 @@ require (
 	github.com/vmihailenco/msgpack/v5 v5.3.4 // indirect
 	github.com/vmihailenco/tagparser v0.1.2 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	github.com/wk8/go-ordered-map/v2 v2.1.8
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	mellium.im/sasl v0.3.1 // indirect
--- main.go
+++ main.go
@@ -1,58 +1,137 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+
+	"github.com/doug-martin/goqu/v9"
+	_ "github.com/doug-martin/goqu/v9/dialect/postgres"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/go-pg/pg/v10"
-	"github.com/go-pg/pg/v10/orm"
+	_ "github.com/lib/pq"
 )
 
 
-type Leads struct {
-	Partner string `json:"partner" pg:"partner"`
-	Users int `json:"users" pg:"users"`
-	Active_Leads  int  `json:"active_leads" pg:"active_leads"`
-	Unassigned_Leads int `json:"unassigned_leads" pg:"unassigned_leads"`
-	Active_Prospect int `json:"active_prospects" pg:"active_prospects"`
-	Active_Opportunity int `json:"active_opportunity" pg:"active_opportunity"`
+
+func MarshalOrdered(data []map[string]interface{}, keys []string) ([]byte, error) {
+	var jsonData []byte
+	jsonData = append(jsonData, '[')
+
+	for i, lead := range data {
+		if i > 0 {
+			jsonData = append(jsonData, ',')
+		}
+
+		jsonData = append(jsonData, '{')
+
+	
+		for j, key := range keys {
+			if j > 0 {
+				jsonData = append(jsonData, ',')
+			}
+			jsonData = append(jsonData, '"')
+			jsonData = append(jsonData, key...)
+			jsonData = append(jsonData, '"', ':')
+
+			val, err := json.Marshal(lead[key])
+			if err != nil {
+				return nil, err
+			}
+			jsonData = append(jsonData, val...)
+		}
+
+		jsonData = append(jsonData, '}')
+	}
+
+	jsonData = append(jsonData, ']')
+
+	return jsonData, nil
 }
 
-func main() {
-	db := loadDatabase()
 
+func main() {
+	
 	router := gin.Default()
 	router.Use(cors.Default())
 
 	router.GET("/leads",func(ctx *gin.Context) {
-		var leads []Leads
-		err := db.Model(&leads).Select()
+
+		dialect := goqu.Dialect("postgres")
+
+		pgDb, err := sql.Open("postgres", "user=shouryagautam password=shourya dbname=postgres sslmode=disable")
 		if err != nil {
+		panic(err.Error())
+		}
+		goqudb := dialect.DB(pgDb)
+
+		rows, err := goqudb.Query(`WITH RECURSIVE employee_hierarchy AS (
+			SELECT employee_id, employee_name, manager_id
+			FROM employees
+			WHERE manager_id IS NULL
+		
+			UNION ALL
+			SELECT e.employee_id, e.employee_name, e.manager_id
+			FROM employees e
+			INNER JOIN employee_hierarchy eh ON e.manager_id = eh.employee_id
+		)
+		SELECT manager_id as manager,employee_id  FROM employee_hierarchy`)
+
+		if err != nil {
+			fmt.Println(err.Error())
 			ctx.JSON(500, gin.H{"error": err.Error()})
 			return
 		}
-		ctx.JSON(200, leads)
+
+		col, err := rows.Columns()
+		if err != nil {
+			fmt.Println(err.Error())
+			ctx.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+
+		var leads []map[string]interface{}
+
+		// Iterate over rows
+		for rows.Next() {
+			// Map to hold the current row's data
+			row := make(map[string]interface{})
+
+			// Slice to hold values of current row
+			values := make([]interface{}, len(col))
+			
+			// Assign each pointer to the corresponding value in the values slice
+			for i := range values {
+				values[i] = new(interface{})
+			}
+
+			// Scan the current row into the pointers
+			if err := rows.Scan(values...); err != nil {
+				fmt.Println(err.Error())
+				continue
+			}
+
+			// Populate the row map with column name and value
+			for i, colName := range col {
+				row[colName] = *(values[i].(*interface{}))
+			}
+
+			// Append the row map to the result
+			leads = append(leads, row)
+		}
+
+		jsonData,err := MarshalOrdered(leads, col)
+
+		if err != nil {
+			fmt.Println(err.Error())
+			ctx.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+
+    	ctx.Data(200, "application/json", jsonData)
 	})
 
 	router.Run(":8080")
 }
 
 
-func loadDatabase() *pg.DB {
-
-	db := pg.Connect(&pg.Options{
-		Addr:     "localhost:5432",
-		User:     "shouryagautam",
-		Password: "shourya",
-		Database: "postgres",
-	})
-
-	err := db.Model((*Leads)(nil)).CreateTable(&orm.CreateTableOptions{
-		IfNotExists: true,
-	})
-
-	if err != nil {
-		panic(err)
-	}
-
-	return db
-}
